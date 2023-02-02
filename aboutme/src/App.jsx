11a8b20f2592e3fb6fc4d6cb6ca15fc929d9b60c
Mark Weiss <markweiss@goldenwest.com>
2023-02-02 13:46:40 -0700

    Beginning to reformat webpage :)

--- aboutme/src/App.jsx
+++ aboutme/src/App.jsx
@@ -5,38 +5,30 @@ import {
     Route
 } from "react-router-dom";
 import Home from './pages/home';
-import Classes from './pages/classes'
-import Resume from './pages/resume'
-import Projects from './pages/projects'
-import {createTheme, ThemeProvider} from '@mui/material/styles';
+import {ThemeProvider, createTheme} from '@mui/material/styles';
+
 
 const theme = createTheme({
     palette: {
-        type: 'light',
+        mode: 'light',
         primary: {
-            main: '#4527a0',
+            main: '#4dd0e1',
         },
         secondary: {
-            main: '#26a69a',
-        },
-        error: {
-            main: '#ff3d00',
+            main: '#b388ff',
         },
     },
 });
 
 function App() {
     return (
-        <ThemeProvider theme={theme}>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/resume" element={<Resume/>}/>
-                    <Route path="/classes" element={<Classes/>}/>
-                    <Route path="/projects" element={<Projects/>}/>
-                </Routes>
-            </Router>
-        </ThemeProvider>
+            <ThemeProvider theme={theme}>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                    </Routes>
+                </Router>
+            </ThemeProvider>
     );
 }
 
---
+++ aboutme/src/tools/Bar.jsx
@@ -0,0 +1,148 @@
+import React from 'react'
+import {Alert, AppBar, Container, IconButton, Menu, MenuItem, Snackbar, Toolbar, Typography} from "@mui/material";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
+import PhoneIcon from '@mui/icons-material/Phone';
+import CloseIcon from '@mui/icons-material/Close';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
+
+const Bar = () => {
+    const [phoneAnchor, setPhoneAnchor] = React.useState(null);
+    const [mailAnchor, setMailAnchor] = React.useState(null);
+    const [snackVal, setSnackVal] = React.useState("")
+    const [snackOpen, setSnackOpen] = React.useState(false)
+    const phoneOpen = Boolean(phoneAnchor);
+    const mailOpen = Boolean(mailAnchor);
+
+    const handlePhoneClick = (event) => {
+        setPhoneAnchor(event.currentTarget);
+    };
+    const handlePhoneClose = () => {
+        setPhoneAnchor(null);
+    };
+
+    const handleMailClick = (event) => {
+        setMailAnchor(event.currentTarget);
+    };
+    const handleMailClose = () => {
+        setMailAnchor(null);
+    };
+
+    function copyToClip(val) {
+        setSnackVal(val)
+        navigator.clipboard.writeText(val).then(() => {
+            setSnackOpen(true)
+        })
+    }
+
+    const closeSnack = (event: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setSnackOpen(false)
+    }
+
+    const action = (
+        <IconButton
+            size="small"
+            aria-label="close"
+            color="inherit"
+            onClick={closeSnack}
+        >
+            <CloseIcon fontSize="small"/>
+        </IconButton>
+    )
+
+    return (
+
+        <AppBar sx={{m: 0, p: 0}}>
+
+            <Toolbar disableGutters>
+                <Typography variant="h6" noWrap component="a" sx={{
+                    ml: 4,
+                    fontFamily: 'monospace',
+                    fontWeight: 700,
+                    letterSpacing: '.3rem',
+                    color: 'inherit',
+                    textDecoration: 'none',
+                }}>
+                    MARK WEISS
+                </Typography>
+                <Typography sx={{flexGrow:1}}/>
+                <IconButton size="small" onClick={() => {
+                    window.open("https://www.linkedin.com/in/mark-weiss-2018/")
+                }}>
+                    <LinkedInIcon fontSize="large"/>
+                </IconButton>
+                <IconButton size="small" onClick={() => {
+                    window.open("https://github.com/WeissTMark")
+                }}>
+                    <GitHubIcon fontSize="large"/>
+                </IconButton>
+                <IconButton size="small" onClick={handleMailClick}>
+                    <EmailIcon fontSize="large"/>
+                </IconButton>
+                <IconButton size="small" onClick={handlePhoneClick} sx={{mr:4}}>
+                    <PhoneIcon fontSize="large"/>
+                </IconButton>
+                <Menu
+                    id="phoneMenu"
+                    anchorEl={phoneAnchor}
+                    open={phoneOpen}
+                    onClose={handlePhoneClose}
+                >
+                    <MenuItem>
+                        <Typography sx={{
+                            mr: 2,
+                            display: {xs: 'none', md: 'flex'},
+                            fontFamily: 'monospace',
+                            fontWeight: 500,
+                            letterSpacing: '.2rem',
+                            color: 'inherit',
+                            textDecoration: 'none'
+                        }}>(303)913-8658</Typography>
+                        <IconButton size="small" onClick={() => {
+                            copyToClip("(303)913-8658")
+                        }}>
+                            <ContentCopyIcon/>
+                        </IconButton>
+                    </MenuItem>
+                </Menu>
+
+                <Menu
+                    id="mailMenu"
+                    anchorEl={mailAnchor}
+                    open={mailOpen}
+                    onClose={handleMailClose}
+                >
+                    <MenuItem>
+                        <Typography sx={{
+                            mr: 2,
+                            display: {xs: 'none', md: 'flex'},
+                            fontFamily: 'monospace',
+                            fontWeight: 500,
+                            letterSpacing: '.2rem',
+                            color: 'inherit',
+                            textDecoration: 'none'
+                        }}>weisstmark@gmail.com</Typography>
+                        <IconButton size="small" onClick={() => {
+                            copyToClip("weisstmark@gmail.com")
+                        }}>
+                            <ContentCopyIcon/>
+                        </IconButton>
+                    </MenuItem>
+                </Menu>
+                <Snackbar open={snackOpen} autoHideDuration={6000} onClose={closeSnack}>
+                    <Alert severity='success' onClose={closeSnack}>
+                        {"'" + snackVal + "' Has been successfully copied to the clipboard."}
+                    </Alert>
+                </Snackbar>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Bar
