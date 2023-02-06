1d6cf72c559cb0bba8e4364734b320a9b642e0b0
Mark Weiss <weisstmark@gmail.com>
2023-02-06 14:45:31 -0700

    Starting to add content

--- aboutme/src/pages/home.jsx
+++ aboutme/src/pages/home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Bar from "../tools/Bar";
+import EP25 from '../files/EP25.jpg'
+import markaxe from '../files/markaxe.jpg'
 import "./home.css"
 import {
     Box,
@@ -120,39 +122,94 @@ const Home = () => {
         {/*    </Grid>*/}
         {/*    <Grid item xs={2}/>*/}
         {/*</Grid>*/}
+        <Grid container justifyContent="center" align="center" spacing={2}>
+            <Grid item xs={12} justifyContent="center">
+                <Typography variant="h1" align='center'>Who is Mark Weiss?</Typography>
+            </Grid>
+            <Grid item xs={8} justifyContent="stretch" alignItems="stretch">
+                <Paper elevation={1} sx={{backgroundColor: 'rgba(215,225,231,0.7)'}}>
+                <Typography variant="h6" sx={{p:2}}>
+                    I am currently a part time Software Developer working at Golden West Telecommunications.
+                    I am also a 5th year student at South Dakota Mines working on my Bachelors degree in Computer Science.
+                    
+                </Typography>
+                </Paper>
+            </Grid>
+            <Grid item xs={12} justifyContent="center">
+                <Typography variant="h1" align='center'>2014-2018</Typography>
+            </Grid>
+            <Grid item xs={10}>
+                <img src={EP25} style={{width: 235, height: 315}}/>
+            </Grid>
+            <Grid item xs={7} justifyContent="stretch" alignItems="stretch">
+                <Paper elevation={1} sx={{backgroundColor: 'rgba(215,225,231,0.7)'}}>
+                <Typography variant="h5" align="left" sx={{p:2}}>
+                    TEAMWORK MAKES THE DREAMWORK
+                </Typography>
+                </Paper>
+            </Grid>
+            <Grid item xs={12}><div style={{height:100}}/></Grid>
+            <Grid item xs={12} justifyContent="center">
+                <Typography variant="h1" align='center'>2018-2019</Typography>
+            </Grid>
+            <Grid item xs={3.5}>
+                <img src={markaxe} style={{width: 400, height: 250}}/>
+            </Grid>
+            <Grid item xs={7} justifyContent="stretch" alignItems="stretch">
+                <Paper elevation={1} sx={{backgroundColor: 'rgba(215,225,231,0.8)', height: 250}}>
+                <Typography variant="h5" align="left" sx={{p:2}}>
+                    This is a description of my high school years! And I am adding a lof of fluff text to make sure it goes to the next line
+                </Typography>
+                </Paper>
+            </Grid>
+        </Grid>
 
-        <ScrollContainer>
+        <div style={{height: 4000}}/>
+
+        {/* <ScrollContainer>
             <ScrollPage>
                 <Animator animation={batch(Fade(), Sticky(), Move(0, 700, 0, -700))}>
-                    <Typography variant="h3">2014 - 2018</Typography>
+                    <Typography variant="h1">2014 - 2018</Typography>
                 </Animator>
             </ScrollPage>
             <ScrollPage>
                 <Animator animation={batch(Fade(), Sticky(), Move(0, 700, 0, -700))}>
-                    <Typography variant="h1">Scroll animation owo<br /> UWUUUUUUU</Typography>
+                    <Grid container>
+                        <Grid item xs={6}>
+                            <img src={painting} style={{width: 300, height: 250}}/>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Typography variant="h6">This is information about my high school years</Typography>
+                        </Grid>
+                    </Grid>
                 </Animator>
             </ScrollPage>
             <ScrollPage>
                 <Animator animation={batch(Fade(), Sticky(), Move(0, 700, 0, -700))}>
-                    <Typography>Scroll animation owo</Typography>
+                    <Typography variant="h1">2018-2019</Typography>
                 </Animator>
             </ScrollPage>
             <ScrollPage>
                 <Animator animation={batch(Fade(), Sticky(), Move(0, 700, 0, -700))}>
-                    <Typography>Scroll animation owo</Typography>
+                    <Typography variant="h1">2019-2020</Typography>
                 </Animator>
             </ScrollPage>
             <ScrollPage>
                 <Animator animation={batch(Fade(), Sticky(), Move(0, 700, 0, -700))}>
-                    <Typography>Scroll animation owo</Typography>
+                    <Typography variant="h1">2020-2021</Typography>
+                </Animator>
+            </ScrollPage>
+            <ScrollPage>
+                <Animator animation={batch(Fade(), Sticky(), Move(0, 700, 0, -700))}>
+                    <Typography variant="h1">2021-2022</Typography>
                 </Animator>
             </ScrollPage>
             <ScrollPage>
                 <Animator animation={batch(Fade(), Sticky(), Move(0, 1000, 0, -1000))}>
-                    <Typography>Scroll animation owo</Typography>
+                    <Typography variant="h1">2022-2023</Typography>
                 </Animator>
             </ScrollPage>
-        </ScrollContainer>
+        </ScrollContainer> */}
 
     </>)
 }
